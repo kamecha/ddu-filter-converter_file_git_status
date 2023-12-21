432899efdfaf8cc6b59a4cc3df893018f3e22d94
kamecha <masakikameyama81@gmail.com>
2023-12-21 17:39:57 +0900

    pathそれぞれにstatusを適用させる仮組み

--- denops/@ddu-filters/converter_file_git.ts
+++ denops/@ddu-filters/converter_file_git.ts
@@ -2,17 +2,37 @@ import { FilterArguments } from "https://deno.land/x/ddu_vim@v3.8.1/base/filter.
 import {
   BaseFilter,
   DduFilterItems,
+  DduItem,
 } from "https://deno.land/x/ddu_vim@v3.8.1/types.ts";
+import { ActionData } from "https://deno.land/x/ddu_kind_file@v0.5.3/file.ts";
+import { parse } from "./git_status.ts";
 
 type Params = Record<never, never>;
 
 export class Filter extends BaseFilter<Params> {
   filter(
-    {}: FilterArguments<Params>,
-  ): DduFilterItems | Promise<DduFilterItems> {
-    throw new Error("Method not implemented.");
+    args: FilterArguments<Params>,
+  ): Promise<DduFilterItems> {
+    return Promise.resolve(
+      args.items.map((item: DduItem) => {
+        const action = item.action! as ActionData;
+        const path = action.path!;
+        const command = new Deno.Command("git", {
+          args: ["status", "--short", path],
+        });
+        const line = new TextDecoder()
+          .decode(command.outputSync().stdout)
+          .trimEnd();
+        const parsed = parse(line);
+        const display = item.display ?? item.word;
+        return {
+          ...item,
+          display: display + `[${parsed?.X}${parsed?.Y}]`,
+        };
+      }),
+    );
   }
   params(): Params {
-    throw new Error("Method not implemented.");
+    return {};
   }
 }
