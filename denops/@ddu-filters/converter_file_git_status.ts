6403ab95a73ae28bc63b2deaf5adab2e34dcb42c
kamecha <masakikameyama81@gmail.com>
2023-12-22 10:37:31 +0900

    highlightを付けるようにした

--- denops/@ddu-filters/converter_file_git_status.ts
+++ denops/@ddu-filters/converter_file_git_status.ts
@@ -3,14 +3,17 @@ import {
   BaseFilter,
   DduFilterItems,
   DduItem,
+  ItemHighlight,
 } from "https://deno.land/x/ddu_vim@v3.8.1/types.ts";
 import { ActionData } from "https://deno.land/x/ddu_kind_file@v0.5.3/file.ts";
-import { parse } from "./git_status.ts";
+import { Entity, parse } from "./git_status.ts";
+
+const HIGHLIGHT_NAME = "ddu_git_status";
 
 type Params = Record<never, never>;
 
 export class Filter extends BaseFilter<Params> {
-  filter(
+  async filter(
     args: FilterArguments<Params>,
   ): Promise<DduFilterItems> {
     for (const item of args.items) {
@@ -28,11 +31,88 @@ export class Filter extends BaseFilter<Params> {
         item.display = display + "    ";
         continue;
       }
+      await args.denops.cmd(
+        "highlight default link ddu_git_status_bracket Comment",
+      );
+      await args.denops.cmd(
+        "highlight default link ddu_git_status_index Special",
+      );
+      await args.denops.cmd(
+        "highlight default link ddu_git_status_worktree WarningMsg",
+      );
+      await args.denops.cmd(
+        "highlight default link ddu_git_status_unmerged ErrorMsg",
+      );
+      await args.denops.cmd(
+        "highlight default link ddu_git_status_untracked Comment",
+      );
+      await args.denops.cmd(
+        "highlight default link ddu_git_status_ignored Comment",
+      );
+      const highlights: ItemHighlight[] = item.highlights ?? [];
+      for (const highlight of this.defineHilight(item, parsed)) {
+        highlights.push(highlight);
+      }
       item.display = display + `[${parsed.X}${parsed.Y}]`;
+      item.highlights = highlights;
     }
     return Promise.resolve(args.items);
   }
   params(): Params {
     return {};
+  }
+  defineHilight(item: DduItem, entity: Entity): ItemHighlight[] {
+    const highlights: ItemHighlight[] = [];
+    const display = item.display ?? item.word;
+    // [
+    highlights.push({
+      name: HIGHLIGHT_NAME,
+      hl_group: "ddu_git_status_bracket",
+      col: display.length + 1,
+      width: 1,
+    });
+    // XY
+    switch (entity.X + entity.Y) {
+      case "??":
+        highlights.push({
+          name: HIGHLIGHT_NAME,
+          hl_group: "ddu_git_status_untracked",
+          col: display.length + 2,
+          width: 2,
+        });
+        break;
+      case "!!":
+        highlights.push({
+          name: HIGHLIGHT_NAME,
+          hl_group: "ddu_git_status_ignored",
+          col: display.length + 2,
+          width: 2,
+        });
+        break;
+      default:
+        // X
+        highlights.push({
+          name: HIGHLIGHT_NAME,
+          hl_group: "ddu_git_status_index",
+          col: display.length + 2,
+          width: 1,
+        });
+        // Y
+        highlights.push({
+          name: HIGHLIGHT_NAME,
+          hl_group: "ddu_git_status_worktree",
+          col: display.length + 3,
+          width: 1,
+        });
+        break;
+    }
+    // ]
+    highlights.push({
+      name: HIGHLIGHT_NAME,
+      hl_group: "ddu_git_status_bracket",
+      col: display.length + 4,
+      width: 1,
+    });
+    return highlights;
   }
 }
