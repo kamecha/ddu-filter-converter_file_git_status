8eeccece99afb0e2f32d2dff9fea96aa06ac8e66
kamecha <masakikameyama81@gmail.com>
2023-12-22 20:04:29 +0900

    ハイライト追加

--- denops/@ddu-columns/file_git_status.ts
+++ denops/@ddu-columns/file_git_status.ts
@@ -3,8 +3,10 @@ import {
   GetTextArguments,
   GetTextResult,
 } from "https://deno.land/x/ddu_vim@v3.8.1/base/column.ts";
-import { ActionData, BaseColumn } from "../deps.ts";
-import { parse } from "../@ddu-filters/git_status.ts";
+import { ActionData, BaseColumn, ItemHighlight } from "../deps.ts";
+import { Entity, parse } from "../@ddu-filters/git_status.ts";
+
+const HIGHLIGHT_NAME = "ddu_git_status";
 
 export type Params = Record<never, never>;
 
@@ -14,9 +16,9 @@ export class Column extends BaseColumn<Params> {
   ): number | Promise<number> {
     return 4;
   }
-  getText(
+  async getText(
     args: GetTextArguments<Params>,
-  ): GetTextResult | Promise<GetTextResult> {
+  ): Promise<GetTextResult> {
     const action = args.item.action! as ActionData;
     const path = action.path!;
     const command = new Deno.Command("git", {
@@ -31,11 +33,83 @@ export class Column extends BaseColumn<Params> {
         text: "    ",
       };
     }
+    await args.denops.cmd(
+      "highlight default link ddu_git_status_bracket Comment",
+    );
+    await args.denops.cmd(
+      "highlight default link ddu_git_status_index Special",
+    );
+    await args.denops.cmd(
+      "highlight default link ddu_git_status_worktree WarningMsg",
+    );
+    await args.denops.cmd(
+      "highlight default link ddu_git_status_unmerged ErrorMsg",
+    );
+    await args.denops.cmd(
+      "highlight default link ddu_git_status_untracked Comment",
+    );
+    await args.denops.cmd(
+      "highlight default link ddu_git_status_ignored Comment",
+    );
     return {
       text: `[${parsed.X}${parsed.Y}]`,
+      highlights: this.defineHilight(args.startCol, parsed),
     };
   }
   params(): Params {
     return {};
+  }
+  defineHilight(startCol: number, entity: Entity): ItemHighlight[] {
+    const highlights: ItemHighlight[] = [];
+    // [
+    highlights.push({
+      name: HIGHLIGHT_NAME,
+      hl_group: "ddu_git_status_bracket",
+      col: startCol,
+      width: 1,
+    });
+    // XY
+    switch (entity.X + entity.Y) {
+      case "??":
+        highlights.push({
+          name: HIGHLIGHT_NAME,
+          hl_group: "ddu_git_status_untracked",
+          col: startCol + 1,
+          width: 2,
+        });
+        break;
+      case "!!":
+        highlights.push({
+          name: HIGHLIGHT_NAME,
+          hl_group: "ddu_git_status_ignored",
+          col: startCol + 1,
+          width: 2,
+        });
+        break;
+      default:
+        // X
+        highlights.push({
+          name: HIGHLIGHT_NAME,
+          hl_group: "ddu_git_status_index",
+          col: startCol + 1,
+          width: 1,
+        });
+        // Y
+        highlights.push({
+          name: HIGHLIGHT_NAME,
+          hl_group: "ddu_git_status_worktree",
+          col: startCol + 2,
+          width: 1,
+        });
+        break;
+    }
+    // ]
+    highlights.push({
+      name: HIGHLIGHT_NAME,
+      hl_group: "ddu_git_status_bracket",
+      col: startCol + 3,
+      width: 1,
+    });
+    return highlights;
   }
 }
